db0f21866f2b4298357b73c7db7927addb129c16
Aleksandr Elizarov <elezar_87@mail.ru>
2020-12-15 17:02:04 +0300

    Added Hello world.

---
+++ Hello world/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println("a > b = "+(a>b));
+        System.out.println("a < b = "+(a<b));
+        String Hello = "Hello";
+        String world = "world!";
+        System.out.println(Hello + " "+ world + "!");
+        System.out.println(!);
+    }
+}
